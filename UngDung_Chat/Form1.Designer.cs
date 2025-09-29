c50aafe9e9d7e365795ea61654cd3e494ec2dea5
vttaicode <nvantai039@gmail.com>
2025-09-29 23:00:48 +0700

    First commit - LAN Chat App

---
+++ UngDung_Chat/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace UngDung_Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtNhapTinNhan = new System.Windows.Forms.TextBox();
+            this.rtbChat = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoClient = new System.Windows.Forms.RadioButton();
+            this.rdoServer = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(27, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(134, 44);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(182, 30);
+            this.txtIP.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(27, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(134, 92);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(182, 30);
+            this.txtPort.TabIndex = 3;
+            // 
+            // txtNhapTinNhan
+            // 
+            this.txtNhapTinNhan.Location = new System.Drawing.Point(332, 28);
+            this.txtNhapTinNhan.Name = "txtNhapTinNhan";
+            this.txtNhapTinNhan.Size = new System.Drawing.Size(344, 22);
+            this.txtNhapTinNhan.TabIndex = 4;
+            // 
+            // rtbChat
+            // 
+            this.rtbChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbChat.Location = new System.Drawing.Point(3, 159);
+            this.rtbChat.Name = "rtbChat";
+            this.rtbChat.Size = new System.Drawing.Size(1014, 310);
+            this.rtbChat.TabIndex = 5;
+            this.rtbChat.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Snow;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnConnect.Location = new System.Drawing.Point(612, 64);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(127, 58);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Snow;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnStart.Location = new System.Drawing.Point(434, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(127, 58);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.Snow;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnSend.Location = new System.Drawing.Point(819, 7);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(127, 58);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(124, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nhập tin nhắn:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtIP);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox1.Location = new System.Drawing.Point(256, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(755, 144);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin địa chỉ";
+            // 
+            // rdoClient
+            // 
+            this.rdoClient.AutoSize = true;
+            this.rdoClient.Location = new System.Drawing.Point(48, 93);
+            this.rdoClient.Name = "rdoClient";
+            this.rdoClient.Size = new System.Drawing.Size(83, 29);
+            this.rdoClient.TabIndex = 11;
+            this.rdoClient.TabStop = true;
+            this.rdoClient.Text = "Client";
+            this.rdoClient.UseVisualStyleBackColor = true;
+            this.rdoClient.CheckedChanged += new System.EventHandler(this.rdoClient_CheckedChanged);
+            // 
+            // rdoServer
+            // 
+            this.rdoServer.AutoSize = true;
+            this.rdoServer.Location = new System.Drawing.Point(48, 45);
+            this.rdoServer.Name = "rdoServer";
+            this.rdoServer.Size = new System.Drawing.Size(91, 29);
+            this.rdoServer.TabIndex = 12;
+            this.rdoServer.TabStop = true;
+            this.rdoServer.Text = "Server";
+            this.rdoServer.UseVisualStyleBackColor = true;
+            this.rdoServer.CheckedChanged += new System.EventHandler(this.rdoServer_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtNhapTinNhan);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 475);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1014, 84);
+            this.panel1.TabIndex = 13;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdoClient);
+            this.groupBox2.Controls.Add(this.rdoServer);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(247, 144);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lựa chọn";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.95069F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.04931F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1014, 150);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.rtbChat, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.05085F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 66.94915F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 89F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1020, 562);
+            this.tableLayoutPanel2.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(1020, 562);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lập trình mạng-nhóm 4-chủ đề 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtNhapTinNhan;
+        private System.Windows.Forms.RichTextBox rtbChat;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoClient;
+        private System.Windows.Forms.RadioButton rdoServer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ UngDung_Chat/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+namespace UngDung_Chat
+{
+    public partial class Form1: Form
+    {
+        TcpClient client;
+        TcpListener server;
+        NetworkStream stream;
+        Thread receiveThread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int port = int.Parse(txtPort.Text);
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            rtbChat.AppendText("Server started...\n");
+            Thread acceptThread = new Thread(() =>
+            {
+                client = server.AcceptTcpClient();
+                stream = client.GetStream();
+                rtbChat.Invoke((MethodInvoker)(() =>
+                    rtbChat.AppendText("Client connected!\n")));
+
+                receiveThread = new Thread(ReceiveData);
+                receiveThread.Start();
+            });
+            acceptThread.Start();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string ip = txtIP.Text;
+            int port = int.Parse(txtPort.Text);
+
+            client = new TcpClient(ip, port);
+            stream = client.GetStream();
+
+            rtbChat.AppendText("Connected to server...\n");
+
+            receiveThread = new Thread(ReceiveData);
+            receiveThread.Start();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (stream != null)
+            {
+                string message = txtNhapTinNhan.Text;
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                rtbChat.AppendText("Me: " + message + "\n");
+                txtNhapTinNhan.Clear();
+            }
+        }
+        private void ReceiveData()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+                    rtbChat.Invoke((MethodInvoker)(() =>
+                        rtbChat.AppendText("Friend: " + message + "\n")));
+                }
+            }
+            catch
+            {
+                rtbChat.Invoke((MethodInvoker)(() =>
+                    rtbChat.AppendText("Disconnected.\n")));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdoServer.Checked = true;
+            Mode();
+            this.AcceptButton = btnSend;
+        }
+
+        private void rdoServer_CheckedChanged(object sender, EventArgs e)
+        {
+            Mode();
+        }
+
+        private void rdoClient_CheckedChanged(object sender, EventArgs e)
+        {
+            Mode();
+        }
+        private void Mode()
+        {
+            if (rdoServer.Checked)
+            {
+                txtIP.Enabled = false;         
+                btnStart.Enabled = true;   
+                btnConnect.Enabled = false;         
+            }
+            else if (rdoClient.Checked)
+            {
+                txtIP.Enabled = true;             
+                btnStart.Enabled = false;    
+                btnConnect.Enabled = true;         
+            }
+        }
+    }
+}
